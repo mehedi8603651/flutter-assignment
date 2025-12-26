feb2a8192416c07077326e83e78edd3085555b39
mehedi8603651 <mehedihasan8603651@gmail.com>
2025-12-27 00:58:00 +0800

    dart 1st assignment set in dart using for loop

---
+++ lib/first_assignment_set_in_dart.dart
@@ -0,0 +1,14 @@
+//first assignment sets in dart
+void main() {
+  Set<int> A = {1, 2, 3, 4, 5};
+  Set<int> B = {4, 5, 6, 7, 8};
+  for (int a in A) {
+    print('set A value: $a');
+  }
+  print('\n');
+  for (int b in B) {
+    print('set B value: $b');
+  }
+
+  print('\nfirst assignment done');
+}
