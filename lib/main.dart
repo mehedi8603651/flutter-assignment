30f541c9074ef4388d0c6a99351f90c8fefa743c
mehedi8603651 <mehedihasan8603651@gmail.com>
2026-03-13 05:10:53 +0800

    Assignment 5: Flutter Assignment - Build a Tasbih Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,156 +1,5 @@
-//------------------------------------------
-
-//1. Increment (+)
-
-//------------------------------------------
-/*import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-*/
-
-//------------------------------------------
-
-//2. Decrement ( − )
-
-//------------------------------------------
-
-/*import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 5;
-
-  void _decrementCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _decrementCounter,
-        tooltip: 'Decrement',
-        child: const Icon(Icons.remove),
-      ),
-    );
-  }
-}*/
-
-//------------------------------------------
-
-//3. Reset Button
-
-//------------------------------------------
-
 import 'package:flutter/material.dart';
+import 'package:flutter_assingmests/text_style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -161,63 +10,219 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _resetcounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        title: Text('Tasbih Counter'),
+        titleSpacing: 5,
+        backgroundColor: const Color(0xFFFFFFFF),
+        leading: Icon(Icons.arrow_back),
+        leadingWidth: 35,
+        actions: [
+          Container(
+            height: 30,
+            width: 30,
+            margin: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xFFCEC5D8).withOpacity(0.76),
             ),
-            IconButton(onPressed: _resetcounter, icon: Icon(Icons.restore)),
-          ],
+            child: Icon(Icons.notifications),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 447,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/tasbih_1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 10),
+                          Text(
+                            'الله أكبر',
+                            style: AppTextStyle.arbiWhiteText20,
+                          ),
+                          SizedBox(height: 10),
+                          Text('0:00:40', style: AppTextStyle.timmer),
+                          SizedBox(height: 10),
+                          Text(
+                            'Tasbih Counter',
+                            style: AppTextStyle.arbiWhiteText20,
+                          ),
+                          SizedBox(height: 10),
+                          Text("046", style: AppTextStyle.timmer),
+                          SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              height: 45,
+                              width: 108,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Color(0xFF794CA5),
+                              ),
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Container(
+                                  height: 39,
+                                  width: 39,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color: Color(0xFFCEC5D8),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 40),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            spacing: 20,
+                            children: [
+                              Container(
+                                height: 35,
+                                width: 44,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                      'assets/images/Reset_Button.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 35,
+                                width: 52,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                      'assets/images/Stop_Button.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 35,
+                                width: 44,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                      'assets/images/Pause_Button.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Add Theme',
+                    style: TextStyle(fontSize: 16, color: Color(0xFF000000)),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  spacing: 16,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 155,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(2),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/Theme_1.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 155,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(2),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/Theme_2.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF764CA5).withOpacity(0.7),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        selectedLabelStyle: AppTextStyle.bottomIconText,
+        unselectedLabelStyle: AppTextStyle.bottomIconText,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/home-outline.png'),
+              size: 24,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/calendar-outline.png'),
+              size: 24,
+            ),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/clock-outline.png'),
+              size: 24,
+            ),
+            label: 'Shedule',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/settings-outline.png'),
+              size: 24,
+            ),
+            label: 'Setting',
+          ),
+        ],
       ),
     );
   }
